5f46d0c480c591ade428794d951e13086e490cb1
tigawanna <denniskinuthiaw@gmail.com>
2026-09-05 12:31:48 +0300

    Add UserInfo fragment and layoutUserPageLoaderQuery for user data retrieval

---
+++ apps/web/src/routes/_dashboard/$user/__generated__/layoutUserPageLoaderQuery.graphql.ts
@@ -0,0 +1,183 @@
+/**
+ * @generated SignedSource<<af0118a493e848c6ecd85e5b5892ff16>>
+ * @lightSyntaxTransform
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type layoutUserPageLoaderQuery$variables = {
+  login: string;
+};
+export type layoutUserPageLoaderQuery$data = {
+  readonly user: {
+    readonly " $fragmentSpreads": FragmentRefs<"UserInfo">;
+  } | null | undefined;
+};
+export type layoutUserPageLoaderQuery = {
+  response: layoutUserPageLoaderQuery$data;
+  variables: layoutUserPageLoaderQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "login"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "login",
+    "variableName": "login"
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*:: as any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "layoutUserPageLoaderQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*:: as any*/),
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "user",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "UserInfo"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*:: as any*/),
+    "kind": "Operation",
+    "name": "layoutUserPageLoaderQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*:: as any*/),
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "user",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "name",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "login",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "email",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "bio",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "avatarUrl",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "company",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "twitterUsername",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "createdAt",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "location",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "url",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "isViewer",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "8850f956918991f54ea01f39957d5646",
+    "id": null,
+    "metadata": {},
+    "name": "layoutUserPageLoaderQuery",
+    "operationKind": "query",
+    "text": "query layoutUserPageLoaderQuery(\n  $login: String!\n) {\n  user(login: $login) {\n    ...UserInfo\n    id\n  }\n}\n\nfragment UserInfo on User {\n  id\n  name\n  login\n  email\n  bio\n  avatarUrl\n  company\n  twitterUsername\n  createdAt\n  location\n  url\n  isViewer\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "bb81b7f1e1924124a02470af10fb9202";
+
+export default node;
