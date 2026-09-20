ccaac3ee97b471f2bf44960609e76f28e87a9cf0
tigawanna <denniskinuthiaw@gmail.com>
2026-09-20 15:15:49 +0300

    Update database configuration and migration journal
    - Changed the database dialect from SQLite to Turso in `drizzle.config.ts` to support new database features.
    - Updated the `.gitignore` to include all environment files while excluding the example file for better clarity.
    - Modified the migration journal to include a new entry, enhancing the tracking of migration history.

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,8 +4,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema/*",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 });
